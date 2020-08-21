e739efa33a32a8481b8c11b3578f919043b5837a
Luis <lm.perezpacheco@gmail.com>
2020-08-21 20:12:38 +0200

    aa

--- js/main.js
+++ js/main.js
@@ -1,19 +1,6 @@
 // Your web app's Firebase configuration
 // Your web app's Firebase configuration
 
-var firebaseConfig = {
-    apiKey: "AIzaSyDc2aK2DuZUW7Iw_WuValYq9o0Y7a_TTTs",
-    authDomain: "test-bar-qrcode.firebaseapp.com",
-    databaseURL: "https://test-bar-qrcode.firebaseio.com",
-    projectId: "test-bar-qrcode",
-    storageBucket: "test-bar-qrcode.appspot.com",
-    messagingSenderId: "1085750921277",
-    appId: "1:1085750921277:web:001764db5c058c11bac254",
-    measurementId: "G-TFXHZDFRFY"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
 
 firebase.auth().signInWithEmailAndPassword("lm.perezpacheco@gmail.com", "5864").then(function (result){
     var user = result.user;
